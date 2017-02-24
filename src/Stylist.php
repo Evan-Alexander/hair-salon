4b66171eed5dfd6b9195c0e18ea25a9fc3426a41
Jason Brown <jasontbrown99@gmail.com>
2017-02-24 14:00:26 -0800

    pass getID for stylist class

--- src/Stylist.php
+++ src/Stylist.php
@@ -1,35 +1,23 @@
 <?php
-    Class stylist
+    Class Stylist
     {
         private $stylist_name;
-        private $client_id;
         private $id;
 
-        function __construct($stylist_name, $client_id, $id = null)
+        function __construct($stylist_name, $id = null)
         {
             $this->stylist_name = $stylist_name;
-            $this->client_id = $client_id;
             $this->id = $id;
         }
 
-        function setStylistName($new_stylist_name)
+        function setStylistName($new_name)
         {
-            $this->stylist_name = $new_stylist_name;
+            $this->stylist_name =  (string) $new_name;
         }
 
         function getStylistName()
         {
             return $this->stylist_name;
-        }
-
-        function setClientId($new_client_id)
-        {
-            $this->client_id = $new_client_id;
-        }
-
-        function getClientId()
-        {
-            return $this->client_id;
         }
 
         function getId()
@@ -39,23 +27,31 @@
 
         function save()
         {
-            $GLOBALS['DB']->exec("INSERT INTO stylist (stylist_name, client_id) VALUES ('{$this->getStylistName()}', {$this->getClientId()})");
+
+            $GLOBALS['DB']->exec("INSERT INTO stylists (stylist_name) VALUES ('{$this->getStylistName()}')");
             $this->id = $GLOBALS['DB']->lastInsertId();
         }
 
-        static function GetAll()
+        static function getAll()
         {
             $returned_stylists = $GLOBALS['DB']->query("SELECT * FROM stylists;");
             $stylists = array();
-            foreach ($returned_stylists as $stylist) {
+            foreach ($returned_stylists as $stylist){
                 $stylist_name = $stylist['stylist_name'];
-                $client_id = $client['client_id'];
                 $id = $stylist['id'];
-                $new_stylist = new Stylist($stylist_name, $client_id, $id);
+                $new_stylist = new Stylist($stylist_name, $id);
                 array_push($stylists, $new_stylist);
             }
             return $stylists;
         }
+
+        static function deleteAll()
+        {
+            $GLOBALS['DB']->exec("DELETE FROM stylists;");
+        }
+
+
+
     }
 
 
