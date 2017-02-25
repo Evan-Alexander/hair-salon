36055354ba77f1ac710c6aeb007faea10217cb93
Jason Brown <jasontbrown99@gmail.com>
2017-02-24 16:11:52 -0800

    pass tests for find, getAll, and deleteAll functions

--- tests/StylistTest.php
+++ tests/StylistTest.php
@@ -108,8 +108,13 @@
 
         function test_save()
         {
+            $client_name = "Joe";
+            $new_client = new Client($client_name);
+            $new_client->save();
+            $client_id = $new_client->getId();
+
             $stylist_name = "Samantha";
-            $new_stylist = new Stylist($stylist_name);
+            $new_stylist = new Stylist($stylist_name, $client_id);
             $new_stylist->save();
 
             $result = Stylist::getAll();
@@ -119,12 +124,17 @@
 
         function test_getAll()
         {
+            $client_name = "Joe";
+            $new_client = new Client($client_name);
+            $new_client->save();
+            $client_id = $new_client->getId();
+
             $stylist1 = "Jeff";
-            $new_stylist1 = new Stylist($stylist1);
+            $new_stylist1 = new Stylist($stylist1, $client_id);
             $new_stylist1->save();
 
             $stylist2 = "Jason";
-            $new_stylist2 = new Stylist($stylist2);
+            $new_stylist2 = new Stylist($stylist2, $client_id);
             $new_stylist2->save();
 
             $result = Stylist::getAll();
@@ -134,11 +144,16 @@
 
         function test_deleteAll()
         {
+            $client_name = "Joe";
+            $new_client = new Client($client_name);
+            $new_client->save();
+            $client_id = $new_client->getId();
+
             $stylist = "Josh";
             $stylist2 = "Kat";
-            $new_stylist = new Stylist($stylist);
+            $new_stylist = new Stylist($stylist, $client_id);
             $new_stylist->save();
-            $new_stylist2 = new Stylist($stylist2);
+            $new_stylist2 = new Stylist($stylist2, $client_id);
             $new_stylist2->save();
 
             Stylist::deleteAll();
@@ -149,10 +164,15 @@
 
         function test_find()
         {
+            $client_name = "Joe";
+            $new_client = new Client($client_name);
+            $new_client->save();
+            $client_id = $new_client->getId();
+
             $stylist = "Jason";
             $stylist2 = "Jaime";
-            $new_stylist = new Stylist($stylist);
-            $new_stylist2 = new Stylist($stylist2);
+            $new_stylist = new Stylist($stylist, $client_id);
+            $new_stylist2 = new Stylist($stylist2, $client_id);
             $new_stylist->save();
             $new_stylist2->save();
 
