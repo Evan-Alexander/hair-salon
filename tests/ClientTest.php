c7a5f53d7896d624ad3f83df057bf5d36cd948e8
Jason Brown <jasontbrown99@gmail.com>
2017-02-26 00:04:31 -0800

    pass test for client id

--- tests/ClientTest.php
+++ tests/ClientTest.php
@@ -14,6 +14,11 @@
 
     class ClientTest extends PHPUnit_Framework_TestCase
     {
+        protected function tearDown()
+        {
+            Client::deleteAll();
+            Stylist::deleteAll();
+        }
 
         function test_setClientName()
         {
@@ -27,6 +32,36 @@
             $result = $new_name->getClientName();
 
             $this->assertEquals($new_client_name, $result);
+        }
+
+        function test_getClientName()
+        {
+            $name = "Fester";
+            $stylist_id = 2;
+            $id = null;
+            $new_name = new Client($name, $stylist_id, $id);
+
+            $result = $new_name->getClientName();
+
+            $this->assertEquals($name, $result);
+        }
+
+        function testgetId()
+        {
+            //Arrange
+            $stylist = "Penny";
+            $id = null;
+            $test_stylist = new Stylist($name, $id);
+            $test_stylist->save();
+
+            $client = "Lane";
+            $stylist_id = $test_stylist->get_Stylist_Id();
+            $test_client = new Client($client, $id, $stylist_id);
+            $test_client->save();
+            //Act
+            $result = $test_client->getId();
+            //Assert
+            $this->assertEquals(true, is_numeric($result));
         }
     }
 
