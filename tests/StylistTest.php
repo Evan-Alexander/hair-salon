4967e446e3674ed4dd6f64ad092172b7b691c5aa
Jason Brown <jasontbrown99@gmail.com>
2017-02-25 18:56:23 -0800

    pass setstylistname test

--- tests/StylistTest.php
+++ tests/StylistTest.php
@@ -23,6 +23,18 @@
             $this->assertEquals($name, $result);
         }
 
+        function test_setStylistName()
+        {
+            $name = "Fester";
+            $new_name = new Stylist($name);
+            $new_stylist_name = "Derrick";
+
+            $new_name->setStylistName($new_stylist_name);
+            $result = $new_name->getStylistName();
+
+            $this->assertEquals($new_stylist_name, $result);
+        }
+
         function test_get_Stylist_Id()
         {
             $stylist_name = "Fred";
@@ -34,16 +46,30 @@
             $this->assertEquals(true, is_numeric($result));
         }
 
-        function test_Save()
-        {
-            $stylist_name = "Dan";
-            $test_stylist = new Stylist($stylist_name);
-            $test_stylist->save();
+        // function test_Save()
+        // {
+        //     $stylist_name = "Dan";
+        //     $test_stylist = new Stylist($stylist_name);
+        //     $test_stylist->save();
+        //
+        //     $result = Stylist::getAll();
+        //
+        //     $this->assertEquals($test_stylist, $result[0]);
+        // }
 
-            $result = Stylist::getAll();
-
-            $this->assertEquals($test_stylist, $result[0]);
-        }
+        // function test_getAll()
+        // {
+        //     $stylist1_name = "Carla";
+        //     $new_stylst = new Stylist($stylist1_name);
+        //     $new_stylst->save();
+        //     $stylist2_name = "Francis";
+        //     $new_stylst2 = new Stylist($stylist2_name);
+        //     $new_stylst2->save();
+        //
+        //     $result = Stylist::getAll();
+        //
+        //     $this->assertEquals([$new_stylst, $new_stylist2], $result);
+        // }
     }
 
 ?>
