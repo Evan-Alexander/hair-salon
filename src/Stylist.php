f4f4bd9818f5a2041a1131356da3ec5f7e8466c6
Jason Brown <jasontbrown99@gmail.com>
2017-02-25 18:16:16 -0800

    erases most everythin.  Stylist class recreated and getStylistName passes

--- src/Stylist.php
+++ src/Stylist.php
@@ -1,20 +1,18 @@
 <?php
-    Class Stylist
+    class Stylist
     {
         private $stylist_name;
-        private $client_id;
-        private $id;
+        private $stylist_id;
 
-        function __construct($stylist_name, $client_id, $id = null)
+        function __construct($stylist_name, $stylist_id = null)
         {
             $this->stylist_name = $stylist_name;
-            $this->client_id = $client_id;
-            $this->id = $id;
+            $this->id = $stylist_id;
         }
 
         function setStylistName($new_name)
         {
-            $this->stylist_name =  (string) $new_name;
+            $this->stylist_name = (string) $new_name;
         }
 
         function getStylistName()
@@ -22,58 +20,9 @@
             return $this->stylist_name;
         }
 
-        function setClientId($new_client_id)
-        {
-            $this->client_id = (int) $new_client_id;
-        }
-
-        function getClientId()
-        {
-            return $this->client_id;
-        }
-
-        function getId()
+        function get_Stylist_Id()
         {
             return $this->id;
-        }
-
-        function save()
-        {
-
-            $GLOBALS['DB']->exec("INSERT INTO stylists (stylist_name, client_id) VALUES ('{$this->getStylistName()}', '{$this->getClientId()}')");
-            $this->id = $GLOBALS['DB']->lastInsertId();
-        }
-
-        static function getAll()
-        {
-            $returned_stylists = $GLOBALS['DB']->query("SELECT * FROM stylists;");
-            $stylists = array();
-            foreach ($returned_stylists as $stylist){
-                $stylist_name = $stylist['stylist_name'];
-                $id = $stylist['id'];
-                $client_id = $stylist['client_id'];
-                $new_stylist = new Stylist($stylist_name, $client_id, $id);
-                array_push($stylists, $new_stylist);
-            }
-            return $stylists;
-        }
-
-        static function deleteAll()
-        {
-            $GLOBALS['DB']->exec("DELETE FROM stylists;");
-        }
-
-        static function find($search_id)
-        {
-            $found_stylist = null;
-            $stylists = Stylist::getAll();
-            foreach($stylists as $stylist) {
-                $stylist_id = $stylist->getId();
-                if ($stylist_id == $search_id) {
-                    $found_stylist = $stylist;
-                }
-            }
-            return $found_stylist;
         }
     }
 ?>
--- tests/StylistTest.php
+++ tests/StylistTest.php
@@ -1,11 +1,9 @@
 <?php
-
     /**
     * @backupGlobals disabled
     * @backupStaticAttributes disabled
     */
 
-    require_once "src/Client.php";
     require_once "src/Stylist.php";
 
     $server = 'mysql:host=localhost:8889;dbname=hair_salon_test';
@@ -13,172 +11,16 @@
     $password = 'root';
     $DB = new PDO($server, $username, $password);
 
-    class StylistTest extends PHPUnit_Framework_TestCase
+    class TaskTest extends PHPUnit_Framework_TestCase
     {
-        protected function tearDown()
-        {
-            Client::deleteAll();
-            Stylist::deleteAll();
-        }
-
-        function test_getId()
-        {
-            // Arrange
-            $client_name = "Carl";
-            $new_client = new Client($client_name);
-            $new_client->save();
-            $client_id = $new_client->getId();
-
-            $id = 1;
-            $stylist_name = "Sam";
-            $new_stylist = new Stylist($stylist_name, $client_id, $id);
-            // Act
-            $result = $new_stylist->getId();
-            // Assert
-            $this->assertEquals($id, $result);
-        }
-
         function test_getStylistName()
         {
-            $client_name = "Joe";
-            $new_client = new Client($client_name);
-            $new_client->save();
-            $client_id = $new_client->getId();
+            $name = "Sam";
+            $test_stylist = new Stylist($name);
 
-            $stylist_name = "Kim";
-            $new_stylist = new Stylist($stylist_name, $client_id);
+            $result = $test_stylist->getStylistName();
 
-            $result = $new_stylist->getStylistName();
-
-            $this->assertEquals($stylist_name, $result);
-        }
-
-        function test_setStylistName()
-        {
-            $client_name = "Joe";
-            $new_client = new Client($client_name);
-            $new_client->save();
-            $client_id = $new_client->getId();
-
-            $stylist_name = "Kim";
-            $new_stylist = new Stylist($stylist_name, $client_id);
-            $new_stylist_name = "Kimberly";
-
-            $new_stylist->setStylistName($new_stylist_name);
-            $result = $new_stylist->getStylistName();
-
-            $this->assertEquals($new_stylist_name, $result);
-        }
-
-        function test_getClientId()
-        {
-            $client_name = "Joe";
-            $new_client = new Client($client_name);
-            $new_client->save();
-            $client_id = $new_client->getId();
-
-            $stylist_name = "Kim";
-            $new_stylist = new Stylist($stylist_name, $client_id);
-
-            $result = $new_stylist->getClientId();
-
-            $this->assertEquals($client_id, $result);
-        }
-
-        function test_setClientId()
-        {
-            $client_name = "Joe";
-            $new_client = new Client($client_name);
-            $new_client->save();
-            $client_id = $new_client->getId();
-
-            $client_name = "Max";
-            $new_client2 = new Client($client_name);
-            $new_client2->save();
-            $client_id2 = $new_client2->getId();
-
-            $stylist_name = "Kim";
-            $new_stylist = new Stylist($stylist_name, $client_id);
-
-            $new_stylist->setClientId($client_id2);
-            $result = $new_stylist->getClientId();
-
-            $this->assertEquals($client_id2, $result);
-        }
-
-        function test_save()
-        {
-            $client_name = "Joe";
-            $new_client = new Client($client_name);
-            $new_client->save();
-            $client_id = $new_client->getId();
-
-            $stylist_name = "Samantha";
-            $new_stylist = new Stylist($stylist_name, $client_id);
-            $new_stylist->save();
-
-            $result = Stylist::getAll();
-
-            $this->assertEquals($new_stylist, $result[0]);
-        }
-
-        function test_getAll()
-        {
-            $client_name = "Joe";
-            $new_client = new Client($client_name);
-            $new_client->save();
-            $client_id = $new_client->getId();
-
-            $stylist1 = "Jeff";
-            $new_stylist1 = new Stylist($stylist1, $client_id);
-            $new_stylist1->save();
-
-            $stylist2 = "Jason";
-            $new_stylist2 = new Stylist($stylist2, $client_id);
-            $new_stylist2->save();
-
-            $result = Stylist::getAll();
-
-            $this->assertEquals([$new_stylist1, $new_stylist2], $result);
-        }
-
-        function test_deleteAll()
-        {
-            $client_name = "Joe";
-            $new_client = new Client($client_name);
-            $new_client->save();
-            $client_id = $new_client->getId();
-
-            $stylist = "Josh";
-            $stylist2 = "Kat";
-            $new_stylist = new Stylist($stylist, $client_id);
-            $new_stylist->save();
-            $new_stylist2 = new Stylist($stylist2, $client_id);
-            $new_stylist2->save();
-
-            Stylist::deleteAll();
-            $result = Stylist::getAll();
-
-            $this->assertEquals([], $result);
-        }
-
-        function test_find()
-        {
-            $client_name = "Joe";
-            $new_client = new Client($client_name);
-            $new_client->save();
-            $client_id = $new_client->getId();
-
-            $stylist = "Jason";
-            $stylist2 = "Jaime";
-            $new_stylist = new Stylist($stylist, $client_id);
-            $new_stylist2 = new Stylist($stylist2, $client_id);
-            $new_stylist->save();
-            $new_stylist2->save();
-
-            $result = Stylist::find($new_stylist2->getId());
-
-            $this->assertEquals($new_stylist2, $result);
+            $this->assertEquals($name, $result);
         }
     }
 
